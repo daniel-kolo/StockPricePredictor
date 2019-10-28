8c862b894823035bffb585d036011fb8e2cd4724
daniel-kolo <kolodani95@gmail.com>
2019-10-28 15:20:28 +0100

    first steps

--- FinanceDataManager.py
+++ FinanceDataManager.py
@@ -7,3 +7,19 @@ class FinanceDataManager:
     def __init__(self, stock_name):
         self.stock = yf.Ticker(stock_name)
 
+    def get_closing_prices(self):
+        df = self.stock.history(period="10y")
+        return df.Close
+
+    def process_data(self):
+        pass
+
+    def get_train_data(self):
+        pass
+
+    def get_test_data(self):
+        pass
+
+    def get_validate_data(self):
+        pass
+
---
+++ LSTM_Network.py
@@ -0,0 +1,4 @@
+class LSTM_Network:
+
+    def __init__(self):
+        pass
