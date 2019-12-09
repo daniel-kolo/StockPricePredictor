b2f77d602e113c9856efbb544baefeb7802cbde8
Daniel <daniel-kolo>
2019-12-09 23:46:20 +0100

    fixed the fucking react dropdown

--- react-is-fun/src/index.js
+++ react-is-fun/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Login from "./containers/Login";
-import axios from 'axios';
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import Login from "./containers/Login"
+import axios from 'axios'
 import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { tsParenthesizedType } from '@babel/types'
 
 class Message extends React.Component{
     render(){
@@ -23,32 +25,45 @@ class Message extends React.Component{
 
 class TickerList extends React.Component{
     constructor() {
-        super()
-    }
-      componentWillMount() {
-        this.getDataAxios()
-    }
-
-    async getDataAxios(){
-        const response =
-          await axios.get("http://localhost:8080/tickerList",
-          { headers: {'Content-Type': 'application/json'}})
-        console.log(response.data)
+        super();
+        this.state = {tickers : []};
+        
     }
     
+    componentWillMount() {
+        axios.get("http://localhost:8080/tickerList",
+        { headers: {'Content-Type': 'application/json'}}).then(res => {
+            let data = res.data[0].split(",")
+            const tickers = data.map((ticker,index) =>
+            <Dropdown.Item key={index} >{ticker}</Dropdown.Item>
+            );
+            this.setState({tickers: tickers});
+          });
+    }
+
     render(){
         return (
-            <div>
-                <p>Hello World</p>
-                
-            </div>
+                <div>
+                    <h1>Choose a stock to predict</h1>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Dropdown Button
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            {this.state.tickers}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    
+                </div>
         )
     }
 }
 
-class Test extends React.Component{
+class DropdownTest extends React.Component{
     constructor() {
         super()
+        this.data = ['asd', 'asd2']
     }
     render(){
         return (
@@ -61,9 +76,9 @@ class Test extends React.Component{
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    {this.data.map(size => (
+                        <Dropdown.Item>{size}</Dropdown.Item>
+                        ))}
                     </Dropdown.Menu>
                 </Dropdown>
                 
@@ -72,10 +87,14 @@ class Test extends React.Component{
     }
 }
 
+
+
+
+
 ReactDOM.render(
     //<Message color="blue" msg="How are you?" minutes={5} />,
     //<Login/>,
     //<TickerList/>,
-    <Test/>,
+    <TickerList/>,
     document.getElementById('root')
 )
